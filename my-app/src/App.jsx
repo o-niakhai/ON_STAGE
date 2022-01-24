e7378479384849600c6060c48aae5002733a7ce3
o-niakhai <o.niakhai@gmail.com>
2022-01-24 13:05:50 +0100

    startBildTheme_commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderAppBar from './components/Header';
+import './App.css';
+
+const App = () => {
+  return (
+    <div >
+      <HeaderAppBar />
+      gndhbfvd
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/styles/theme.jsx
@@ -0,0 +1,9 @@
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles';
+
+const theme = createMuiTheme();
+
+const useStyles = makeStyles((theme) => {
+  root: {
+    // some CSS that access to theme
+  }
+});
