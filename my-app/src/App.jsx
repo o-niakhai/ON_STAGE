45fe45b1f2c9b12acededa62d9a4f8e5b43098bc
o-niakhai <o.niakhai@gmail.com>
2022-01-26 20:57:33 +0100

    Header commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './components/Header';
+import MiniDrawer from './components/Header';
 import './App.css';
 
 
 const App = () => {
   return (
     <div >
-      <Header />
+      <MiniDrawer />
     </div>
   );
 }
