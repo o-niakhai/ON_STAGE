92f1541aac63510fb12175e1a7a3bcdcab8d00d0
o-niakhai <o.niakhai@gmail.com>
2022-01-30 22:43:07 +0100

    riderBox

--- my-app/src/components/CooperationComponents/Riders.jsx
+++ my-app/src/components/CooperationComponents/Riders.jsx
@@ -2,70 +2,47 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-
-function Item(props) {
-    const { sx, ...other } = props;
-    return (
-        <Box
-            sx={{
-                bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#F5F6FA' : 'grey.100'),
-                color: (theme) => (theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800'),
-                border: '1px solid',
-                borderColor: (theme) =>
-                    theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
-                ...sx,
-            }}
-            {...other}
-        />
-    );
-}
-
-Item.propTypes = {
-    sx: PropTypes.oneOfType([
-        PropTypes.arrayOf(
-            PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool]),
-        ),
-        PropTypes.func,
-        PropTypes.object,
-    ]),
-};
+import { Container, Link } from '@mui/material';
+import LocalActivityIcon from '@mui/icons-material/LocalActivity';
+import KingBedIcon from '@mui/icons-material/KingBed';
 
 export default function Riders() {
     return (
-        <div style={{ width: '100%' }}>
+        <Box>
+            <Typography textAlign="left">Rider</Typography>
             <Box
-                sx={{ display: 'flex', flexDirection: 'column'}}
-            >
-                <Typography >Rider</Typography>
-                <Item sx={{ order: 2 }}>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            width: 160,
-                            height: 160
-                        }}>
-                        <Box
-                            sx={{
-                                width: 160,
-                                height: 160,
-                            }}>
-                                <Typography>
-                                    Concert Rider
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'colum',
+                    justifyContent: 'space-between',
+                    width: 180,
+                    height: 180,
+                }}>
+                <Container
+                    sx={{
+                        backgroundColor: '#F5F6FA',
+                        width: '100%',
+                        height: '170',
+                        p: 1
+                    }}>
+                    <LocalActivityIcon sx={{ fontSize: 120 }} />
+                    <Typography>
+                        Concert Rider
                                 </Typography>
-                        </Box>
-                        <Box
-                            sx={{
-                                width: 160,
-                                height: 160,
-                            }}>
-                                <Typography>
-                                    Accommodation
-                                </Typography>
-                        </Box>
-                    </Box>
-                </Item>
+                </Container>
+                <Container
+                    sx={{
+                        backgroundColor: '#F5F6FA',
+                        width: '170',
+                        height: '170',
+                        p: 1
+                    }}>
+                    <KingBedIcon sx={{ fontSize: 120 }} />
+                    <Typography>
+                        Accommodation
+                                    </Typography>
+                </Container>
             </Box>
-        </div>
+        </Box>
     );
 }
