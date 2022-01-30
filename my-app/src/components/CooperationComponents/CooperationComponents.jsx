03eb2620fef5c51c6bb1c4285b5092684d58fc0b
o-niakhai <o.niakhai@gmail.com>
2022-01-30 22:42:35 +0100

    containerRiders commit

--- my-app/src/components/CooperationComponents/CooperationComponents.jsx
+++ my-app/src/components/CooperationComponents/CooperationComponents.jsx
@@ -8,7 +8,6 @@ import Riders from '../CooperationComponents/Riders'
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
