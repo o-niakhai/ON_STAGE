3ce35299379ea0d0e5bc77d7870f2692363fe8e2
o-niakhai <o.niakhai@gmail.com>
2022-02-16 20:05:55 +0100

    commit

--- my-app/src/components/Cooperation/Rider.jsx
+++ my-app/src/components/Cooperation/Rider.jsx
@@ -2,21 +2,34 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import LocalActivityIcon from '@mui/icons-material/LocalActivity';
+import { Box } from '@mui/material';
 
 export default function Rider() {
     return (
-        <Grid 
+        <Box
             sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                alignItems: 'flex-end',
                 backgroundColor: '#F5F6FA',
                 boxShadow: 2,
                 borderRadius: 2,
                 width: 170,
-                height: 140
+                height: 140,
             }}>
-            <LocalActivityIcon sx={{ fontSize: 50 }} />
-            <Typography>
+            <LocalActivityIcon
+                sx={{
+                    fontSize: 50,
+                }} />
+            <Typography
+                sx={{
+                    borderTop: 1,
+                    borderTopColor: '#36385E'
+                }}>
                 Concert Rider
             </Typography>
-        </Grid>
+        </Box>
     );
 }
