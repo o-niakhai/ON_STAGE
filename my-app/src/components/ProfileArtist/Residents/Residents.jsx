9560fc7142d73fcacbacb4d1c25e78f86f8e9e12
o-niakhai <o.niakhai@gmail.com>
2022-02-19 15:26:43 +0100

    Residents commit

--- my-app/src/components/ProfileArtist/Residents/Residents.jsx
+++ my-app/src/components/ProfileArtist/Residents/Residents.jsx
@@ -1,75 +1,25 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import ImageList from '@material-ui/core/ImageList';
-import ImageListItem from '@material-ui/core/ImageListItem';
-import ImageListItemBar from '@material-ui/core/ImageListItemBar';
-import IconButton from '@material-ui/core/IconButton';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import itemData from './itemData';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Image from 'mui-image'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  imageList: {
-    flexWrap: 'nowrap',
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: 'translateZ(0)',
-  },
-  title: {
-    color: theme.palette.primary.light,
-  },
-  titleBar: {
-    background:
-      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-  },
-}));
 
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const itemData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
- */
 export default function Residents() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <ImageList className={classes.imageList} cols={2.5}>
-        {itemData.map((item) => (
-          <ImageListItem key={item.img}>
-            <img src={item.img} alt={item.title} />
-            <ImageListItemBar
-              title={item.title}
-              classes={{
-                root: classes.titleBar,
-                title: classes.title,
-              }}
-              actionIcon={
-                <IconButton aria-label={`star ${item.title}`}>
-                  <StarBorderIcon className={classes.title} />
-                </IconButton>
-              }
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
-    </div>
+    <Box>
+        <Typography variant="h5" textAlign="left">
+            Residents
+        </Typography>
+        <Box
+            sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            p: 1,
+            m: 1}}>
+          <Image src='../../../Media/Club1.jpeg' />
+          <Image src='../../../Media/Club2.jpeg' />
+          <Image src='../../../Media/Club3.jpeg' />
+        </Box>
+    </Box>
   );
 }
