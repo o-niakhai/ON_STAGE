20530a5a8f41ea518aa86a8e93824b86426e2720
o-niakhai <o.niakhai@gmail.com>
2022-02-20 04:45:25 +0100

    Residents commit

--- my-app/src/components/ProfileArtist/Residents/Residents.jsx
+++ my-app/src/components/ProfileArtist/Residents/Residents.jsx
@@ -1,28 +1,31 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Club1 from '../../../Media/Club1.jpeg'
-import Club2 from '../../../Media/Club2.jpeg'
-import Club3 from '../../../Media/Club3.jpeg'
+import Club1 from '../../../Media/Club1.jpg'
+import Club2 from '../../../Media/Club2.jpg'
+import Club3 from '../../../Media/Club3.jpg'
 
 
 
 export default function Residents() {
   return (
     <Box>
-        <Typography variant="h5" textAlign="left">
-            Residents
+      <Typography variant="h5" textAlign="left">
+        Residents
         </Typography>
-        <Box
-            sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            p: 1,
-            m: 1}}>
-          <img src={Club1} />;
-          <img src={Club2} />;
-          <img src={Club3} />
-        </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          height: 150,
+          p: 1,
+          m: 1
+        }}>
+        <img src={Club1} alt='club' />
+        <img src={Club2} alt='club' />
+        <img src={Club3} alt='club' />
+      </Box>
     </Box>
   );
 }
