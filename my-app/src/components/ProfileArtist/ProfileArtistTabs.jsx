8804dc740ded0f9d2f447092f4575d6415ac5942
o-niakhai <o.niakhai@gmail.com>
2022-02-21 15:54:57 +0100

    ResidentsTab commit

--- my-app/src/components/ProfileArtist/ProfileArtistTabs.jsx
+++ my-app/src/components/ProfileArtist/ProfileArtistTabs.jsx
@@ -1,61 +1,111 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import AddCommentRoundedIcon from '@mui/icons-material/AddCommentRounded';
 import FeaturedPlayListRoundedIcon from '@mui/icons-material/FeaturedPlayListRounded';
 import BusinessCenterRoundedIcon from '@mui/icons-material/BusinessCenterRounded';
 import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
 import ScreenShareRoundedIcon from '@mui/icons-material/ScreenShareRounded';
-import Cooperation from './Cooperation/Cooperation';
 import About from './About/About';
 import Message from './Message/Message';
+import Cooperation from './Cooperation/Cooperation';
 import Residents from './Residents/Residents';
 
-export default function ProfileArtistTabs() {
-    const [value, setValue] = React.useState('1');
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+export default function BasicTabs() {
+    const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
     return (
-        <Box sx={{ width: '100%', typography: 'body1' }}>
-            <TabContext value={value}>
-                <Box mr={2}
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'flex-end'
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange}
+                    TabIndicatorProps={{
+                        sx: {
+                            bgcolor: "#45D083",
+                            height: "2px"
+                        }
                     }}>
-                    <TabList
-                        onChange={handleChange}
-                        aria-label="lab API tabs example"
-                        TabIndicatorProps={{
-                            sx: {
-                                bgcolor: "#45D083",
-                                height: "2px"
-                            }
-                        }}>
-                        <Tab style={{ fontSize: '0.7rem', color: '#949494' }}
-                            icon={<FeaturedPlayListRoundedIcon />} iconPosition="start" value="About" label="About" />
-                        <Tab style={{ fontSize: '0.7rem', color: '#949494' }}
-                            icon={<AddCommentRoundedIcon />} iconPosition="start" value="Message" label="Message" />
-                        <Tab style={{ fontSize: '0.7rem', color: '#949494' }}
-                            icon={<BusinessCenterRoundedIcon />} iconPosition="start" value="Cooperation" label="Cooperation" />
-                        <Tab style={{ fontSize: '0.7rem', color: '#949494' }}
-                            icon={<BadgeRoundedIcon />} iconPosition="start" value="Residents" label="Residents" />
-                        <Tab style={{ fontSize: '0.7rem', color: '#949494' }}
-                            icon={<ScreenShareRoundedIcon />} iconPosition="start" value="Share" label="Share" />
-                    </TabList>
-                </Box>
-                <TabPanel value="About"><About /></TabPanel>
-                <TabPanel value="Message"><Message /></TabPanel>
-                <TabPanel value="Cooperation"><Cooperation /></TabPanel>
-                <TabPanel value="Residents"><Residents /></TabPanel>
-                <TabPanel value="Share">Share</TabPanel>
-            </TabContext>
+                    <Tab
+                        label="About" {...a11yProps(0)}
+                        style={{ fontSize: '0.7rem', color: '#949494' }}
+                        icon={<FeaturedPlayListRoundedIcon />}
+                        iconPosition="start"
+                        value="About"
+                    />
+                    <Tab
+                        label="Message" {...a11yProps(1)}
+                        style={{ fontSize: '0.7rem', color: '#949494' }}
+                        icon={<AddCommentRoundedIcon />}
+                        iconPosition="start"
+                        value="Message"
+                    />
+                    <Tab
+                        label="Cooperation" {...a11yProps(2)}
+                        style={{ fontSize: '0.7rem', color: '#949494' }}
+                        icon={<BusinessCenterRoundedIcon />}
+                        iconPosition="start"
+                        value="Cooperation"
+                    />
+                    <Tab
+                        label="Residents" {...a11yProps(3)}
+                        style={{ fontSize: '0.7rem', color: '#949494' }}
+                        icon={<BadgeRoundedIcon />}
+                        iconPosition="start"
+                        value="Residents"
+                    />
+                </Tabs>
+            </Box>
+            <TabPanel value={About} index={0}>
+                <About />
+            </TabPanel>
+            <TabPanel value={Message} index={1}>
+                <Message />
+            </TabPanel>
+            <TabPanel value={Cooperation} index={2}>
+                <Cooperation />
+            </TabPanel>
+            <TabPanel value={Residents} index={3}>
+                <Cooperation />
+            </TabPanel>
         </Box>
     );
 }
