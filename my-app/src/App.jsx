7789e0ad99530d40c717da2a1b19fa9d24e4d607
o-niakhai <o.niakhai@gmail.com>
2022-02-24 22:50:58 +0100

    News commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import Content from '../src/components/Content'
+import ContentArtist from './components/ContentArtist'
 import './App.css';
 
 
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div >
       <Header />
-      <Content />
+      <ContentArtist />
     </div>
   );
 }
