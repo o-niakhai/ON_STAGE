aab1a5ee107aea16031e1d5a0b92fed61f8760f1
Oleg <o.niakhai@gmail.com>
2022-08-26 01:57:08 +0200

    c

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router';
-import {Header} from './components/Header';
-import  {ContentArtist}  from './components/ContentArtist';
-import  {News}  from './components/News';
-import  {Calendar}  from './components/Calendar';
+import { Header } from './components/Header';
+import { ContentArtist }  from './components/ContentArtist';
+import { News }  from './components/News';
+import { Calendar }  from './components/Calendar';
 import './App.css';
 
 
