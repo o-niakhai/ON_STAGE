815ecbd41018a6aa57955102e897518ee8cdaea3
Oleg <o.niakhai@gmail.com>
2022-09-01 14:02:27 +0200

    c

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,7 +3,8 @@ import { Routes, Route } from 'react-router';
 import ContentArtist   from './components/ContentArtist';
 import News from './components/News';
 import CalendarArtist from './components/CalendarArtist';
-import Header from './components/Header'
+import Header from './components/Header';
+import Layout from './components/Layout';
 import './App.css';
 
 
@@ -11,7 +12,7 @@ const App = () => {
   return (
     <div >
       <Routes>
-        <Route path='/' element={<Header/>}>
+        <Route path='/' element={<Layout/>}>
           <Route path="./components/ContentArtist.jsx" element={<ContentArtist />} />
           <Route path="./components/News.jsx" element={<News />} />
           <Route path="./components/CalendarArtist.jsx" element={<CalendarArtist />} />
--- my-app/src/components/Layout.jsx
+++ my-app/src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router';
+import { Link, Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
