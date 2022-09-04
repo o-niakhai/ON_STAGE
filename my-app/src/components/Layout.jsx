3662df481030dc52ed9a64d6d8c57ef008fafd11
Oleg <o.niakhai@gmail.com>
2022-09-05 00:29:15 +0200

    setActive

--- my-app/src/components/Layout.jsx
+++ my-app/src/components/Layout.jsx
@@ -2,15 +2,16 @@ import {  Outlet, NavLink } from "react-router-dom";
 import React from 'react';
 import SideBar from "./SideBar";
 
+const setActive = ({isActive}) => isActive ? 'active-link' : '';
 
 const Layout = () => {
     return (
         <>
             <SideBar>
-                <NavLink to="/" className={({isActive}) => isActive ? 'active-link': ''}>Home</NavLink>
-                <NavLink to="/News">News</NavLink>
-                <NavLink to="/calendar">Calendar</NavLink>
-                <NavLink to="/components/">ContentArtist</NavLink>
+                <NavLink to="/" className={setActive}>Home</NavLink>
+                <NavLink to="/News" className={setActive}>News</NavLink>
+                <NavLink to="/calendar" className={setActive}>Calendar</NavLink>
+                <NavLink to="/components/" className={setActive}>ContentArtist</NavLink>
             </SideBar>
             <main className="container">
                 <Outlet/>
