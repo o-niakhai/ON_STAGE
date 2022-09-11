bd573d07dae1688c224184a61e62e0fb60a87c30
Oleg <o.niakhai@gmail.com>
2022-09-12 01:52:33 +0200

    c

--- my-app/src/components/ProfileArtist/Calendar/Calendar.jsx
+++ my-app/src/components/ProfileArtist/Calendar/Calendar.jsx
@@ -1,71 +1,14 @@
 import React from "react";
-import "antd/dist/antd.css";
-import './styles.css';
-import { Calendar, Alert, Modal } from "antd";
-import moment from "moment";
+import DatePicker from "react-datepicker";
 
-
-export default class CalendarComp extends React.Component {
-  state = {
-    value: moment("2022-09-06"),
-    selectedValue: moment("2022-09-06"),
-    isModalVisible: false
-  };
-
-  onSelect = (value) => {
-    this.setState({
-      value,
-      selectedValue: value,
-      isModalVisible: true
-    });
-  };
-
-  onPanelChange = (value) => {
-    this.setState({ value });
-  };
-
-  onClickTest = () => {
-    console.log("click");
-  };
-
-  handleOk = (value) => {
-    this.setState({
-      isModalVisible: false
-    });
-    this.fullCell(value);
-  };
-
-  handleCancel = () => {
-    this.setState({
-      isModalVisible: false
-    });
-  };
-
-  dateCellRender = (value) => {
-    return <p>jjj</p>;
-  };
-
-  render() {
-    const { value, selectedValue, isModalVisible } = this.state;
-    return (
-      <>
-        <Alert
-          message={`You selected date: ${
-            selectedValue && selectedValue.format("YYYY-MM-DD")
-          }`} />
-        <Calendar
-          value={value}
-          onSelect={this.onSelect}
-          onPanelChange={this.onPanelChange}
-          dateCellRender={this.dateCellRender} />
-        <Modal
-          title="Input your plan"
-          visible={isModalVisible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel} >
-          <input type="text" name="" id="" />
-        </Modal>
-      </>
-    );
-  }
+export default function News() {
+  return (
+    <div >
+      <DatePicker 
+        controls={['calendar']}
+        min="1920-01-01"
+        max="2050-01-01"
+      />
+    </div>
+  );
 }
