8449b1c79cdf8cd9770962cd6b3228e557c89f74
Oleg <o.niakhai@gmail.com>
2022-09-16 01:57:24 +0200

    C

--- my-app/src/components/ProfileArtist/Calendar/Calendar.jsx
+++ my-app/src/components/ProfileArtist/Calendar/Calendar.jsx
@@ -1,14 +1,20 @@
-import React from "react";
-import DatePicker from "react-datepicker";
+import * as React from 'react';
+ import DatePicker from "react-datepicker";
+//import  useState  from 'react';
+import Calendar from 'react-calendar';
 
 export default function Calendar () {
+  //const [value, onChange] = useState(new Date());
   return (
     <>
-      <DatePicker 
+       <DatePicker 
         controls={['calendar']}
         min="1920-01-01"
-        max="2050-01-01"
-      />
+        max="2050-01-01" /> 
+        
+        <Calendar 
+        onChange={onChange} 
+        value={value} />
     </>
   );
 }
