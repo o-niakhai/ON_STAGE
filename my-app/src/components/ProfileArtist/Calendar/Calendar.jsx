9e9efb97652ca7f6cbaa8756cca208bf341d213b
Oleg <o.niakhai@gmail.com>
2022-09-18 08:44:09 +0200

    c

--- my-app/src/components/ProfileArtist/Calendar/Calendar.jsx
+++ my-app/src/components/ProfileArtist/Calendar/Calendar.jsx
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-import Box from '@mui/material/Box';
+import moment from 'moment';
+import CalendarMonthView from "react-calendar-month-view";
 
 export default function CalendarCmp () {
-  const [value, onChange] = useState(new Date());
-  return (
-    <Box 
-    sx={{
-      display: 'flexbox',
-      alignItems: 'stretch',
-      width: '100%',
-      height: '100%',
-      "&:hover": {
-        backgroundColor: 'rgba(0, 0, 0, 0.01)'
-      },}}>
-        <Calendar 
-        onChange={onChange} 
-        value={value}
+  const _renderDay = (day) => {
+    const date = moment(day);
+
+    const diff = date.diff(moment().startOf("day"));
+
+    if (diff === -259200000 || diff === 259200000) {
+      return (
+        <div
+          style={{
+            boxSizing: "border-box",
+            height: "100%",
+            width: "100%",
+            backgroundImage:
+              "url(https://farm2.staticflickr.com/1203/1475793643_d911a66735_m.jpg)",
+            backgroundSize: "cover"
+            // borderRadius: showDefaultStyles ? 0 : '10%'
+          }}
         />
-    </Box>
+      );
+    }
+  };
+  return (
+    <div className="CalendarCmp">
+      <CalendarMonthView renderDay={_renderDay} />
+    </div>
   );
 }
