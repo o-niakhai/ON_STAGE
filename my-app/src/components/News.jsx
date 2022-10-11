b38dffd967adcf0a2bfe3bc3509a5e01da8d776e
Oleg <o.niakhai@gmail.com>
2022-10-11 19:41:15 +0200

    c

--- my-app/src/components/News.jsx
+++ my-app/src/components/News.jsx
@@ -7,6 +7,30 @@ export default function News() {
       <Typography>
           News page
       </Typography>
+      <Box>
+        <Box  display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}>
+        <Box gridColumn="span 2">
+          <Item>
+            
+          </Item>
+        </Box>
+        <Box gridColumn="span 2">
+          <Item>
+            
+          </Item>
+        </Box>
+        <Box gridColumn="span 2">
+          <Item>
+            
+          </Item>
+        </Box>
+        <Box gridColumn="span 2">
+          <Item>
+            
+          </Item>
+        </Box>
+        </Box>
+      </Box>
     </div>
   );
 }
