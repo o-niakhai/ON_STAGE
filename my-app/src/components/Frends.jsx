fc1c9841296a0078c2776a0643fab5f8154c0b8a
Oleg <o.niakhai@gmail.com>
2022-10-13 18:44:41 +0200

    c

--- my-app/src/components/Frends.jsx
+++ my-app/src/components/Frends.jsx
@@ -5,13 +5,12 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Checkbox from '@mui/material/Checkbox';
-import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 
 export default function CheckboxListSecondary() {
   const [checked, setChecked] = React.useState([1]);
 
-  const handleToggle = (value: number) => () => {
+  const handleToggle = (value) => () => {
     const currentIndex = checked.indexOf(value);
     const newChecked = [...checked];
 
@@ -26,10 +25,9 @@ export default function CheckboxListSecondary() {
 
   return (
     <List dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {[0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11].map((value) => {
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => {
         const labelId = `checkbox-list-secondary-label-${value}`;
         return (
-          <Box>
           <ListItem
             key={value}
             secondaryAction={
@@ -46,13 +44,12 @@ export default function CheckboxListSecondary() {
               <ListItemAvatar>
                 <Avatar
                   alt={`Avatar n°${value + 1}`}
-                  src={`./${value + 1}.jpg`}
+                  src={`/static/images/avatar/${value + 1}.jpg`}
                 />
               </ListItemAvatar>
               <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
             </ListItemButton>
           </ListItem>
-          </Box>
         );
       })}
     </List>
