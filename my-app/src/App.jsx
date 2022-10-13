1646d5d87dc3d9769287be1ec7a3fc3bbf66638d
Oleg <o.niakhai@gmail.com>
2022-10-13 18:08:11 +0200

    с

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css';
 import ContentArtist from './components/ContentArtist';
 import News from './components/NewsCom/News';
 import Frends from './components/Frends';
+import Calendar from './components/ProfileArtist/Calendar/Calendar';
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="Home" element={<ContentArtist/>}/>
           <Route path="News" element={<News/>}/>
           <Route path="Frends" element={<Frends/>}/>
+          <Route path="Calendar" element={<Calendar/>}/>
       </Routes>
     </div>
   );
--- my-app/src/components/Frends.jsx
+++ my-app/src/components/Frends.jsx
@@ -1,12 +1,60 @@
-import React from 'react';
-import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Checkbox from '@mui/material/Checkbox';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
 
-export default function Frends()  {
+export default function CheckboxListSecondary() {
+  const [checked, setChecked] = React.useState([1]);
+
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
   return (
-    <div >
-      <Typography>
-          Frends page
-      </Typography>
-    </div>
+    <List dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {[0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11].map((value) => {
+        const labelId = `checkbox-list-secondary-label-${value}`;
+        return (
+          <Box>
+          <ListItem
+            key={value}
+            secondaryAction={
+              <Checkbox
+                edge="end"
+                onChange={handleToggle(value)}
+                checked={checked.indexOf(value) !== -1}
+                inputProps={{ 'aria-labelledby': labelId }}
+              />
+            }
+            disablePadding
+          >
+            <ListItemButton>
+              <ListItemAvatar>
+                <Avatar
+                  alt={`Avatar n°${value + 1}`}
+                  src={`./${value + 1}.jpg`}
+                />
+              </ListItemAvatar>
+              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+            </ListItemButton>
+          </ListItem>
+          </Box>
+        );
+      })}
+    </List>
   );
 }
