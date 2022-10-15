8fb3e0e70e6baae67f2d2b79ee979674b98746c7
Oleg <o.niakhai@gmail.com>
2022-10-15 15:33:19 +0200

    с

--- my-app/src/components/NewsCom/Post.jsx
+++ my-app/src/components/NewsCom/Post.jsx
@@ -1,144 +1,61 @@
 import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Avatar from '@mui/joy/Avatar';
 import Box from '@mui/joy/Box';
 import Card from '@mui/joy/Card';
-import CardOverflow from '@mui/joy/CardOverflow';
-import Link from '@mui/joy/Link';
-import IconButton from '@mui/joy/IconButton';
-import Input from '@mui/joy/Input';
+import CardCover from '@mui/joy/CardCover';
+import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
-import MoreHoriz from '@mui/icons-material/MoreHoriz';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import ModeCommentOutlined from '@mui/icons-material/ModeCommentOutlined';
-import SendOutlined from '@mui/icons-material/SendOutlined';
-import Face from '@mui/icons-material/Face';
-import BookmarkBorderRoundedIcon from '@mui/icons-material/BookmarkBorderRounded';
 
 export default function Post() {
   return (
-    <Card
-      variant="outlined"
-      sx={{
-        minWidth: 300,
-        '--Card-radius': (theme) => theme.vars.radius.xs,
-      }}>
-      <Box sx={{ display: 'flex', alignItems: 'center', pb: 1.5, gap: 1 }}>
-        <Box
-          sx={{
-            position: 'relative',
-            '&:before': {
-              content: '""',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              bottom: 0,
-              right: 0,
-              m: '-2px',
-              borderRadius: '50%',
-              background:
-                'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)',
-            },
-          }}
-        >
-          <Avatar
-            size="sm"
-            src="/static/logo.png"
-            sx={{ p: 0.5, border: '2px solid', borderColor: 'background.body' }}
+    <Box
+      component="ul"
+      sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', p: 0, m: 0 }}
+    >
+      <Card component="li" sx={{ minWidth: 300, flexGrow: 1 }}>
+        <CardCover>
+          <img
+            src="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800"
+            srcSet="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800&dpr=2 2x"
+            loading="lazy"
+            alt=""
           />
-        </Box>
-        <Typography fontWeight="lg">MUI</Typography>
-        <IconButton variant="plain" color="neutral" size="sm" sx={{ ml: 'auto' }}>
-          <MoreHoriz />
-        </IconButton>
-      </Box>
-      <CardOverflow>
-        <AspectRatio>
-          <img src=".../Media/Club1.jpg" alt="" loading="lazy" />
-        </AspectRatio>
-      </CardOverflow>
-      <Box sx={{ display: 'flex', alignItems: 'center', mx: -1, my: 1 }}>
-        <Box sx={{ width: 0, display: 'flex', gap: 0.5 }}>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <FavoriteBorder />
-          </IconButton>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <ModeCommentOutlined />
-          </IconButton>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <SendOutlined />
-          </IconButton>
-        </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5, mx: 'auto' }}>
-          {[...Array(5)].map((_, index) => (
-            <Box
-              key={index}
-              sx={{
-                borderRadius: '50%',
-                width: `max(${6 - index}px, 3px)`,
-                height: `max(${6 - index}px, 3px)`,
-                bgcolor: index === 0 ? 'primary.solidBg' : 'background.level3',
-              }}
+        </CardCover>
+        <CardContent>
+          <Typography
+            level="h6"
+            fontWeight="lg"
+            textColor="#fff"
+            mt={{ xs: 12, sm: 18 }}
+          >
+            Image
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card component="li" sx={{ minWidth: 300, flexGrow: 1 }}>
+        <CardCover>
+          <video
+            autoPlay
+            loop
+            muted
+            poster="https://assets.codepen.io/6093409/river.jpg"
+          >
+            <source
+              src="https://assets.codepen.io/6093409/river.mp4"
+              type="video/mp4"
             />
-          ))}
-        </Box>
-        <Box sx={{ width: 0, display: 'flex', flexDirection: 'row-reverse' }}>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <BookmarkBorderRoundedIcon />
-          </IconButton>
-        </Box>
-      </Box>
-      <Link
-        component="button"
-        underline="none"
-        fontSize="sm"
-        fontWeight="lg"
-        textColor="text.primary"
-      >
-        8.1M Likes
-      </Link>
-      <Typography fontSize="sm">
-        <Link
-          component="button"
-          color="neutral"
-          fontWeight="lg"
-          textColor="text.primary"
-        >
-          MUI
-        </Link>{' '}
-        The React component library you always wanted
-      </Typography>
-      <Link
-        component="button"
-        underline="none"
-        fontSize="sm"
-        startDecorator="…"
-        sx={{ color: 'text.tertiary' }}
-      >
-        more
-      </Link>
-      <Link
-        component="button"
-        underline="none"
-        fontSize="10px"
-        sx={{ color: 'text.tertiary', my: 0.5 }}
-      >
-        2 DAYS AGO
-      </Link>
-      <CardOverflow sx={{ p: 'var(--Card-padding)', display: 'flex' }}>
-        <IconButton size="sm" variant="plain" color="neutral" sx={{ ml: -1 }}>
-          <Face />
-        </IconButton>
-        <Input
-          variant="plain"
-          size="sm"
-          placeholder="Add a comment…"
-          sx={{ flexGrow: 1, mr: 1, '--Input-focusedThickness': '0px' }}
-        />
-        <Link disabled underline="none" role="button">
-          Post
-        </Link>
-      </CardOverflow>
-    </Card>
+          </video>
+        </CardCover>
+        <CardContent>
+          <Typography
+            level="h6"
+            fontWeight="lg"
+            textColor="#fff"
+            mt={{ xs: 12, sm: 18 }}
+          >
+            Video
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
