be05921f65fcac68ddd6f4fa71ef16b783b44594
Oleg <o.niakhai@gmail.com>
2022-11-12 13:16:20 +0100

    c

--- my-app/src/components/ProfileArtist/ProfileArtistTabs.jsx
+++ my-app/src/components/ProfileArtist/ProfileArtistTabs.jsx
@@ -40,7 +40,6 @@ const ProfileArtistTabs = props => {
     history.push(`/${tabNameToIndex[newValue]}`);
     setSelectedTab(newValue);
   };
-
     return (
         <Box sx={{ width: '100%' }}>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
